d283d51cf89ce75d166be2faff9cf7ca4465bc53
Sonja Parsell <hello@sparsell.com>
2021-10-06 15:54:20 -0400

    updating to only create one set of grades

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
+
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # leila = Child.create(name: "Leila", user_id: aya.id, grade_id: five.id)
 #     Device.create(device_type: "iPad")
 
-parkway = School.create(name: "Parkway")
-glenville = School.create(name: "Glenville")
-dundee = School.create(name: "Dundee")
-western = School.create(name: "Western")
-eastern = School.create(name: "Eastern")
-central = School.create(name: "Central")
+parkway = School.create(school_name: "Parkway")
+glenville = School.create(school_name: "Glenville")
+dundee = School.create(school_name: "Dundee")
+western = School.create(school_name: "Western")
+eastern = School.create(school_name: "Eastern")
+central = School.create(school_name: "Central")
 
-grade1p = Grade.create(name: "1st Grade", school: parkway)
-grade2p = Grade.create(name: "2nd Grade", school: parkway)
-grade3p = Grade.create(name: "3rd Grade", school: parkway)
-grade4p = Grade.create(name: "4th Grade", school: parkway)
-grade5p = Grade.create(name: "5th Grade", school: parkway)
+grade1 = Grade.create(grade_name: "1st Grade")
+grade2 = Grade.create(grade_name: "2nd Grade")
+grade3 = Grade.create(grade_name: "3rd Grade")
+grade4 = Grade.create(grade_name: "4th Grade")
+grade5 = Grade.create(grade_name: "5th Grade")
+grade6 = Grade.create(grade_name: "6th Grade")
+grade7 = Grade.create(grade_name: "7th Grade")
+grade8 = Grade.create(grade_name: "8th Grade")
 
-grade1g = Grade.create(name: "1st Grade", school: glenville)
-grade2g = Grade.create(name: "2nd Grade", school: glenville)
-grade3g = Grade.create(name: "3rd Grade", school: glenville)
-grade4g = Grade.create(name: "4th Grade", school: glenville)
-grade5g = Grade.create(name: "5th Grade", school: glenville)
+thomas = Student.create(
+    name: "Thomas", 
+    has_phone: false, 
+    has_other_device: true, 
+    school_id: parkway.id, 
+    grade_id: grade4.id)
+
+charlotte = Student.create(
+    name: "Charlotte", 
+    has_phone: true, 
+    has_other_device: true, 
+    school_id: western.id, 
+    grade_id: grade6.id)
