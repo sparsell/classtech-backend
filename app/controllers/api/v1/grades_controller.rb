cac39df9924a0a8e1ad29af189ba5540ef0a4a66
Sonja Parsell <hello@sparsell.com>
2021-10-07 13:46:26 -0400

    set up grade controller

--- app/controllers/api/v1/grades_controller.rb
+++ app/controllers/api/v1/grades_controller.rb
@@ -1,3 +1,18 @@
 class Api::V1::GradesController < ApplicationController
 
+    def index 
+        @grades = Grade.all 
+        render json: @grades 
+    end
+
+    def show
+        @grade = Grade.find(params[:id])
+    end
+
+    private
+
+        def grade_params
+            params.require(:grade).permit(:grade_name)
+        end
+
 end
