bf398264d88e75d62a9206ec52cadfbc21768f31
Sonja Parsell <hello@sparsell.com>
2021-10-12 15:42:10 -0400

    experimenting with routes and fetch

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,22 @@
 Rails.application.routes.draw do
-  
+
     namespace :api do 
         namespace :v1 do 
-          
             resources :schools do
                 resources :grades do
-                    resources :students
+                    resources :students do
+                          resources :comments
+                    end
                 end
             end
         end 
     end
-    
+
+    namespace :api do 
+        namespace :v1 do 
+            resources :students do
+                          resources :comments
+                    end
+                end
+            end        
 end
