94c316a8e361730398218fb392e2b5bca99e46e9
Sonja Parsell <hello@sparsell.com>
2021-10-13 16:39:17 -0400

    simplified routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,12 +11,5 @@ Rails.application.routes.draw do
             end
         end 
     end
-
-    namespace :api do 
-        namespace :v1 do 
-            resources :students do
-                          resources :comments
-                    end
-                end
-            end        
+   
 end
