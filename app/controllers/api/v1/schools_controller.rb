0d272ba99c8fd50826ec00e37cf8141cd6e5cab5
Sonja Parsell <hello@sparsell.com>
2021-10-13 16:40:39 -0400

    moved create action to notes as stretch feature

--- app/controllers/api/v1/schools_controller.rb
+++ app/controllers/api/v1/schools_controller.rb
@@ -4,16 +4,6 @@ class Api::V1::SchoolsController < ApplicationController
         @schools = School.all 
         render json: @schools
     end
-    
-    def create
-        @school = School.new(school_params)
-        if @school.save
-            render json: @school
-            alert("Successfull added a new school.")
-        else
-            render json: {error: "Error creating account"}
-        end
-    end
 
     def show
         @school = School.find(params[:id])
