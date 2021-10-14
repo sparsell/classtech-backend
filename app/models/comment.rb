fe3bd7ad039004993b344ef175fd7460a95f3ef5
Sonja Parsell <hello@sparsell.com>
2021-10-14 16:06:19 -0400

    removed comments model

--- app/models/comment.rb
+++
@@ -1,5 +0,0 @@
-class Comment < ApplicationRecord
-
-        belongs_to :student
-
-end
--- app/serializers/comment_serializer.rb
+++
@@ -1,4 +0,0 @@
-class SchoolSerializer
-  include JSONAPI::Serializer
-  attributes :screen_time_comment, :student_id
-end
