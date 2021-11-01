465a45c00a570811c58e5098877ff202bbb4828e
Sonja Parsell <hello@sparsell.com>
2021-11-01 10:04:14 -0400

    removed a space

--- app/controllers/api/v1/grades_controller.rb
+++ app/controllers/api/v1/grades_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::GradesController < ApplicationController
 
     def index 
         @grades = Grade.all 
-        render json: @grades , each_serializer: GradeSerializer
+        render json: @grades, each_serializer: GradeSerializer
     end
 
     def show
