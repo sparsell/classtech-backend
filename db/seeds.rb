e060370f7c418a6080c971a44afc338f80f5a416
Sonja Parsell <hello@sparsell.com>
2021-11-11 16:37:29 -0500

    removed 1st grade from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,7 @@
 
 # leila = Child.create(name: "Leila", user_id: aya.id, grade_id: five.id)
 #     Device.create(device_type: "iPad")
-
-grade1 = Grade.create(grade_name: "1st Grade")
+grade2 = Grade.create(grade_name: "2nd Grade")
 grade2 = Grade.create(grade_name: "2nd Grade")
 grade3 = Grade.create(grade_name: "3rd Grade")
 grade4 = Grade.create(grade_name: "4th Grade")
@@ -15,35 +14,35 @@ grade6 = Grade.create(grade_name: "6th Grade")
 grade7 = Grade.create(grade_name: "7th Grade")
 grade8 = Grade.create(grade_name: "8th Grade")
 
-thomas = Student.create(
-    name: "Thomas", 
-    school: "Parkway",
-    has_phone: false, 
-    has_other_device: true,
-    screen_time: "Thomas has one hour of screen time every week day. More on the weekends. He is allowed to text and facetime his friends.", 
-    grade_id: grade4.id)
+# thomas = Student.create(
+#     name: "Thomas", 
+#     school: "Parkway",
+#     has_phone: false, 
+#     has_other_device: true,
+#     screen_time: "Thomas has one hour of screen time every week day. More on the weekends. He is allowed to text and facetime his friends.", 
+#     grade_id: grade4.id)
    
 
-charlotte = Student.create(
-    name: "Charlotte", 
-    school: "Western",
-    has_phone: true, 
-    has_other_device: true, 
-    screen_time: "Charlotte has one hour of screen time every week day. More on the weekends.",
-    grade_id: grade6.id)
+# charlotte = Student.create(
+#     name: "Charlotte", 
+#     school: "Western",
+#     has_phone: true, 
+#     has_other_device: true, 
+#     screen_time: "Charlotte has one hour of screen time every week day. More on the weekends.",
+#     grade_id: grade6.id)
    
-    harper = Student.create(
-    name: "Harper", 
-    school: "Western",
-    has_phone: false, 
-    has_other_device: false, 
-    screen_time: "Harper has no limits!",
-    grade_id: grade6.id)
+#     harper = Student.create(
+#     name: "Harper", 
+#     school: "Western",
+#     has_phone: false, 
+#     has_other_device: false, 
+#     screen_time: "Harper has no limits!",
+#     grade_id: grade6.id)
 
-    tara = Student.create(
-    name: "Tara", 
-    school: "Central",
-    has_phone: true, 
-    has_other_device: false, 
-    screen_time: "Tara can text and call and FaceTime and game on her PC. She's always on her phone and that is fine with me.", 
-    grade_id: grade6.id)
+#     tara = Student.create(
+#     name: "Tara", 
+#     school: "Central",
+#     has_phone: true, 
+#     has_other_device: false, 
+#     screen_time: "Tara can text and call and FaceTime and game on her PC. She's always on her phone and that is fine with me.", 
+#     grade_id: grade6.id)
